e5405f99ecb0064d6998ee0830cd1cb40ab35e8a
wndenis <wndenis@yandex.ru>
2020-05-03 00:48:58 +0300

    Add auto update and downloading logic (front)

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,7 @@ var $fileInput = $('.file-input');
 var $droparea = $(".file-drop-area");
 
 
-var $form = $("#form");
 var $results = $("#results");
-var $resultsElements = $("#results-elements")
 var $button = $("#button");
 var $dragndrop = $("#dragndrop");
 var $filename = $("#filename");
@@ -49,80 +47,6 @@ $fileInput.on('change', function() {
     }
 });
 
-const wait_result_div = "<div class='spinner'><div></div><div></div></div>";
-const ok_result_div = "<span class=\"animated zoomIn\"><span style=\"color: forestgreen\">&#10004;</span></span>";
-const bad_result_div = "<span class=\"animated zoomIn\"><span style=\"color: darkred\">&#10006;</span></span>";
-var listEntries = [];
-
-class ListEntry{
-    state;
-    text;
-    desc;
-    constructor(root, text, desc) {
-        let elem = document.createElement("li");
-        this.text = text;
-        this.documentID = root.appendChild(elem);
-        this.state = 0;
-        this.desc = desc;
-        this.update();
-        listEntries.push(this);
-    }
-
-    get state(){
-        return this.state;
-    }
-
-    setState(newState){
-        this.state = newState;
-        this.update();
-    }
-
-    update(){
-        let newAppearance = "";
-        let newText = this.text;
-        switch (this.state) {
-            case 0:
-            {
-                newAppearance = wait_result_div;
-                break;
-            }
-            case 1:
-            {
-                newAppearance = ok_result_div;
-                break;
-            }
-            case 2:
-            {
-                newAppearance = bad_result_div;
-                newText += " - " + this.desc;
-                break;
-            }
-        }
-        this.documentID.innerHTML = newAppearance + separator + newText;
-    }
-
-}
-
-
-function fillList() {
-    let root = document.getElementById("results-elements");
-    for (let i = 0; i < names.length; i++){
-        setTimeout(() => addElementToList(root, names[i], descs[i]), i * 5);
-        //todo: remove timeout
-    }
-}
-
-function addElementToList(root, item, desc){
-    new ListEntry(root, item, desc);
-}
-
-// 0 - wait
-// 1 - ok
-// 2 - bad
-function setDotAppearance(dotInd, newstate){
-    listEntries[dotInd].setState(newstate);
-}
-
 
 
 $button.click(function(e) {
@@ -146,27 +70,6 @@ $newfile.click(function(e) {
 
 
 // ========================================================================
-let names = [
-    "Шрифт",
-    "Поля",
-    "Названия таблиц",
-    "Названия рисунков",
-    "Заголовки"
-];
-
-let descs = [
-    "должен быть Times New Roman, 14",
-    "должны быть: слева - 3 см, остальные - 2 см",
-    "должны быть выполнены слева сверху над таблицей",
-    "должны быть по центру под рисунком",
-    "должны быть по центру без точки на конце"
-];
-
-fillList(names, descs);
-
-// 0 - wait
-// 1 - ok
-// 2 - bad
 
 function SendFile() {
     // https://demo2398178.mockable.io
@@ -182,43 +85,36 @@ function SendFile() {
         contentType: false,
         success: function (data, status, _) {
             if (typeof data === "string") {
-                data = JSON.parse(data);
+                setTimeout(() => GetFile(data, filenameStr), 1700);
             }
-            let ok = true;
-            for (let i = 0; i < data.length; i++){
-                if (data[i] === 2){
-                    ok = false;
-                }
-                setTimeout(() => listEntries[i].setState(data[i]), i * 200);
-            }
-            setTimeout(() => updateBackground(ok), data.length * 200 + 150)
-
         },
         error: function (xhr, textStatus, errorThrown) {
-            if (textStatus === "BAD REQUEST"){
+            if (textStatus === "BAD REQUEST" || xhr.responseText === "invalid file") {
                 // TODO: сделать красивое сообщение
                 alert("Отправленный вами файл не соответствует требованиям")
-            }
-            else{
+                setTimeout(() => TimeoutUpdate(), 500);
+            } else {
                 console.log(errorThrown);
                 console.log(textStatus);
                 alert("Ошибка. Скорее всего, это проблемы с соединением.");
             }
             //это для обновления страницы
-            location.reload(false);
-            return false;
+            //location.reload(false);
+            //return false;
             //============================
         },
         timeout: 15000
     });
 }
 
+function GetFile(filename, downloadName) {
+    window.open (window.location.href + "getfile?name=" + filename + "&downloadName=" + downloadName);
+    setTimeout(() => TimeoutUpdate(), 500);
 
-function updateBackground(ok) {
-    if (ok){
-        $resultBg.addClass("green-bg")
-    }
-    else{
-        $resultBg.addClass("red-bg")
+}
+
+function TimeoutUpdate(){
+    if ($button.hasClass('hidden')){
+        $newfile.click();
     }
 }
